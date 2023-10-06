8d222bb33d15421005e054a549b66fc2db6ee621
Garrett Nelson <gjnels@proton.me>
2023-10-06 18:37:44 -0400

    Add board helper functions for determining the state of the board

--- src/lib/classes/board.ts
+++ src/lib/classes/board.ts
@@ -22,4 +22,66 @@ export class Board {
     })
     console.log('%c' + formattedString, 'color: lightgreen;font-size:16px')
   }
+
+  // determine if entire board is blank
+  isEmpty() {
+    return this.state.every((cell) => !cell)
+  }
+
+  // determine if entire board is filled
+  isFull() {
+    return this.state.every((cell) => cell)
+  }
+
+  // determine if board is in a terminal state
+  // 1. a player has won the game with 3 markers in a row
+  // 2. the board is full and no one has won
+  isTerminal() {
+    // board cannot be terminal if it is empty
+    if (this.isEmpty()) return false
+
+    // check for horizontal win
+    // row 1
+    if (this.state[0] && this.state[0] === this.state[1] && this.state[0] === this.state[2]) {
+      return { winner: this.state[0], direction: 'H', row: 1 }
+    }
+    // row 2
+    if (this.state[3] && this.state[3] === this.state[4] && this.state[3] === this.state[5]) {
+      return { winner: this.state[3], direction: 'H', row: 2 }
+    }
+    // row 3
+    if (this.state[6] && this.state[6] === this.state[7] && this.state[6] === this.state[8]) {
+      return { winner: this.state[6], direction: 'H', row: 3 }
+    }
+
+    // check for vertical win
+    // column 1
+    if (this.state[0] && this.state[0] === this.state[3] && this.state[0] === this.state[6]) {
+      return { winner: this.state[0], direction: 'V', column: 1 }
+    }
+    // column 2
+    if (this.state[1] && this.state[1] === this.state[4] && this.state[1] === this.state[7]) {
+      return { winner: this.state[1], direction: 'V', column: 2 }
+    }
+    // column 3
+    if (this.state[2] && this.state[2] === this.state[5] && this.state[2] === this.state[8]) {
+      return { winner: this.state[2], direction: 'V', column: 3 }
+    }
+
+    // check for diagonal win
+    // top-left to bottom-right
+    if (this.state[0] && this.state[0] === this.state[4] && this.state[0] === this.state[8]) {
+      return { winner: this.state[0], direction: 'D', diagonal: 'main' }
+    }
+    // top-right to bottom-left
+    if (this.state[2] && this.state[2] === this.state[4] && this.state[2] === this.state[6]) {
+      return { winner: this.state[2], direction: 'D', diagonal: 'counter' }
+    }
+
+    // there is no winner, game is a tie if all cells are full
+    if (this.isFull()) return { winner: 'draw' }
+
+    // there is no winner and the board is not full
+    return false
+  }
 }
